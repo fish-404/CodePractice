3f642bb899e84e9a0d6b2f64de174f397ad8fc7d
fish <fish404hsif@gmail.com>
2021-01-06 15:03:02 +0800

    Update Matched Brackets.py

--- CodeChef/Others/Matched Bracket/Matched Brackets.py
+++ CodeChef/Others/Matched Bracket/Matched Brackets.py
@@ -8,17 +8,17 @@ pairs = 0
 for i in range(n):
     if brackets[i] == 1:
         depth += 1
-        if pairs == 0:
-            pairsOccur = i + 1
+        if pairs == 0: # parent opening bracket
+            pairsOccur = i + 1 # record occurence
         pairs += 1
         if maxPairs < pairs:
             maxPairs = pairs
-            maxPairsOccur = pairsOccur
+            maxPairsOccur = pairsOccur # update occurence
         if nestDepth < depth:
             nestDepth = depth
             nestOccur = i + 1
     else:
         depth -= 1
         if depth == 0:
-            pairs = 0
+            pairs = 0 # clear pairs temp count
 print(nestDepth, nestOccur, maxPairs*2, maxPairsOccur)
