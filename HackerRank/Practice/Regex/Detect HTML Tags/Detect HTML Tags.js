ccef8f0564cb40d8b543f42da778c0f43fdffe52
fish <fish404hsif@gmail.com>
2021-01-18 16:58:03 +0800

    Create Detect HTML Tags.js

---
+++ HackerRank/Practice/Regex/Detect HTML Tags/Detect HTML Tags.js
@@ -0,0 +1,30 @@
+function getTag(fragment) {
+    let pattern = /<\/[a-z][a-z|\d]*>|<[a-z][^<>]*\/>/g
+    let tags = fragment.match(pattern);
+    let tagSet = new Set(tags);
+    let result = new Set();
+    for (let tag of tagSet.keys()) {
+        pattern = /[a-z][a-z|\d]*/g;
+        letter = tag.match(pattern);
+        result.add(letter[0]);
+    }
+    let resultTag = Array.from(result)
+    console.log(resultTag.sort().join(";"));
+}
+
+function processData(input) {
+    //Enter your code here
+    t = input[0];
+    getTag(input);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
