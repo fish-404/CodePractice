59d4b31c9c61ae9ae86dd108088ad04384353d53
fish <fish404hsif@gmail.com>
2021-04-13 14:12:49 +0800

    Update elevatorSaga.js

--- elevatorSaga/elevatorSaga.js
+++ elevatorSaga/elevatorSaga.js
@@ -1,16 +1,26 @@
+/*
+ * Solution to Elevator Saga Game
+ * Game Link: http://play.elevatorsaga.com/
+ * Status: Pending
+ */
+
 {
     init: function(elevators, floors) {
+        // Traverse elevators
         for (let elevator of elevators) {
+            /* Triggerd when elevator has no task */
             elevator.on("idle", function() {
                 elevator.goToFloor(0, true);
             });
             
+            /* Triggerd when someone pressed floor button inside elevator */
             elevator.on("floor_button_pressed", function(floorNum) {
                 elevator.destinationQueue.push(floorNum);
                 elevator.destinationQueue = elevator.destinationQueue.sort();
                 elevator.checkDestinationQueue();
             });
             
+            /* Triggered when elevator passing a floor */
             elevator.on("passing_floor", function(floorNum, direction) {
                 if (direction == "up") {
                     elevator.goingDownIndicator(false);
@@ -20,6 +30,7 @@
                 }
             });
             
+            /* Triggered when elevator stops at a floor */
             elevator.on("stopped_at_floor", function(floorNum) {
                 if (elevator.getPressedFloors().length == 0) {
                     elevator.goingUpIndicator(true);
@@ -31,7 +42,9 @@
             });
         }
 
+        // Traverse floors 
         for (let floor of floors) {
+            /* Triggerd when someone press up botton */
             floor.on("up_button_pressed", function() {
                 for (elevator of elevators) {
                     if (elevator.destinationDirection() == "up") {
@@ -51,6 +64,8 @@
                     }
                 }
             });
+            
+            /* Triggerd when someone press down botton */
             floor.on("down_button_pressed", function() {
                 for (elevator of elevators) {
                     if (elevator.destinationDirection() == "down") {
