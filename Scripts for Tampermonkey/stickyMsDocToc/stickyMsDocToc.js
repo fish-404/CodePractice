e94db06c41ccc59b3ebe695947952dcebeddbf1b
fish <fish404hsif@gmail.com>
2022-05-09 13:14:10 +0800

    fix: limit height for short TOC
    use `max-height` replace `height` to avoid too long block for short TOC

--- Scripts for Tampermonkey/stickyMsDocToc/stickyMsDocToc.js
+++ Scripts for Tampermonkey/stickyMsDocToc/stickyMsDocToc.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         stickyMsDocToc
 // @namespace    mailto:fish404hsif@gmail.com
-// @version      0.2
+// @version      0.3
 // @description  Sticky Microsoft Documents right sidebar TOC block
 // @description:zh 固定微软文档右侧的目录栏
 // @author       Fish__404
@@ -15,7 +15,7 @@
     'use strict';
     try {
       document.getElementById("affixed-right-container").setAttribute("style", "position: sticky; top: 0");
-      document.getElementById("right-rail-in-this-article-list").setAttribute("style", "overflow: auto; height: 80vh;");
+      document.getElementById("right-rail-in-this-article-list").setAttribute("style", "overflow: auto; max-height: 80vh;");
     }
     catch {} // not all page has right sidebar TOC block
 })();
