128df49900371c809ff9baa3ba914047f3467fe9
fish <fish404hsif@gmail.com>
2022-05-09 10:17:19 +0800

    Update stickyMsDocToc.js

--- Scripts for Tampermonkey/stickyMsDocToc/stickyMsDocToc.js
+++ Scripts for Tampermonkey/stickyMsDocToc/stickyMsDocToc.js
@@ -3,7 +3,10 @@
 // @namespace    mailto:fish404hsif@gmail.com
 // @version      0.1
 // @description  Sticky Microsoft Documents right sidebar TOC block
+// @description:zh 固定微软文档右侧的目录栏
 // @author       Fish__404
+// @updateURL    https://github.com/fish-404/CodePractice/blob/main/Scripts%20for%20Tampermonkey/stickyMsDocToc/stickyMsDocToc.js
+// @downloadURL  https://github.com/fish-404/CodePractice/blob/main/Scripts%20for%20Tampermonkey/stickyMsDocToc/stickyMsDocToc.js
 // @match        *://docs.microsoft.com/*
 // @grant        none
 // ==/UserScript==
